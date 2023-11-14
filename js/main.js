45e7c45006b26bc5387e4bade42bfb10039cd9f0
AndreRongcales19 <302334829@student.rocmondriaan.nl>
2023-11-14 12:55:18 +0100

    1 tot met 5 gemaakt

---
+++ js/main.js
@@ -0,0 +1,48 @@
+console.log("main.js werkt");
+
+//opdr 1 
+let person = {
+    firstName: "Hubert Andre",
+    lastName: "Rongcales", 
+    age: 19, 
+    course: "ICT", 
+    score: 7.5,
+}
+
+//opdr 2 
+let personOnFb = {
+    username: "Jamir", 
+    title: "unknown", 
+    caption: "show my love for you",
+    date: "02-11-2023",
+    views: 6200, 
+}
+
+//opdr 3
+let food = {
+    title: "adobo",
+    description: "chicken soup with soy sauce",
+    gezond: "good for your muscle growth",
+}
+
+// opdr 4 
+
+let MercedesCLA = {
+    mileAge: 60000,
+    yearBuilt: 2017,
+    engineSize: 1.6, 
+    description: "Mercedes CLA-Klasse 180 1.6 90KW Blue Eff. Edit. 2017",
+    price: 24000, 
+}
+
+//opdr 5 
+
+let item1 = {
+    title: "1 de Beste Elstar",
+    prijs: 2.19,
+    actie: 1.18, 
+    description: "Met 1 de Beste Elstar heb je altijd een appeltje voor de dorst.",
+    ingredients: "appels", 
+}
+
+// opdr 6 
